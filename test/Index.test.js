18353ff66b8eb3664a24ce072bbfd8e82699b436
Erasmus <refugedesigns17@gmail.com>
2023-09-28 08:51:01 +0000

    wrote 3 tests

--- test/Index.test.js
+++ test/Index.test.js
@@ -1,6 +1,8 @@
 const Xprez = require("../lib/index");
 const assert = require("assert");
 const axios = require("axios");
+const cors = require("cors")
+const bodyParser = require("body-parser");
 
 describe("Xprez", () => {
   let server;
@@ -23,6 +25,64 @@ describe("Xprez", () => {
       assert.equal(response.data, "Hello, world");
     } catch (error) {
       throw error;
+    }
+  });
+
+  it("works with real Express middleware (CORS)", async function () {
+    const app = new Xprez();;
+    app.use(cors());
+    app.use((req, res, next) => {
+      res.end("Hello with CORS");
+      next();
+    });
+    server = app.listen(3000);
+    const res = await axios.get("http://localhost:3000");
+    // This is the header that `cors()` should set
+    assert.equal(res.headers["access-control-allow-origin"], "*");
+    assert.equal(res.data, "Hello with CORS");
+  });
+
+  it("should parse request body correctly", async () => {
+    const app = new Xprez();
+    app.use(bodyParser.json());
+
+    app.post("/", (req, res, next) => {
+      res.end(JSON.stringify(req.body));
+      next();
+    });
+
+    server = app.listen(3000);
+
+    try {
+      const response = await axios.post("http://localhost:3000", {
+        message: "Hello, world",
+      });
+
+      assert.deepEqual(response.data, { message: "Hello, world" });
+    } catch (error) {
+      throw error;
+    } 
+  });
+
+  it("should handle URL parameters correctly", async () => {
+    const app = new Xprez();
+
+    app.get("/greet/:name", (req, res, next) => {
+      const name = req.params.name;
+ 
+      res.end(`Hello, ${name}!`);
+      next();
+    });
+
+    const server = app.listen(3000);
+
+    try {
+      const response = await axios.get("http://localhost:3000/greet/John");
+      assert.equal(response.data, "Hello, John!");
+    } catch (error) {
+      throw error;
+    } finally {
+      server.close();
     }
   });
 });
